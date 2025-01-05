6f109b8e54e260f7b42ec7c6e366a149813b04d4
Klimentin0 <meshkov.klim@gmail.com>
2025-01-06 00:42:37 +0300

    немного магии хэндлеров для проверки количества гет запросов

--- main.go
+++ main.go
@@ -1,20 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	const port = "8080"
+	const root = "."
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(root)))))
+	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("/reset", apiCfg.resetCountHandler)
+	mux.HandleFunc("/count", apiCfg.reqCountHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("serving on port: %s\n", port)
+	log.Printf("serving data from %s on port: %s\n", root, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) reqCountHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Number of hits %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetCountHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Number of hits %d", cfg.fileserverHits.Load())))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
