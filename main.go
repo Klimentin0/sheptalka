16999c52b2225b190026f2ba87f035812516522f
Klimentin0 <meshkov.klim@gmail.com>
2025-01-08 19:44:02 +0300

    настройка под постгрес(с sqlc)

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Klimentin0/sheptalka/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	queries        *database.Queries
+}
+
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	dbQueries := database.New(db)
+
 	const port = "8080"
 	const root = "."
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		queries:        dbQueries,
 	}
 
 	mux := http.NewServeMux()
@@ -28,8 +50,4 @@ func main() {
 
 	log.Printf("serving data from %s on port: %s\n", root, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
 }
